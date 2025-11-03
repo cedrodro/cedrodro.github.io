f36f301fec20067938483dd8ec9c6c0e47964314
cedrodro <78496086+cedrodro@users.noreply.github.com>
2025-11-03 17:40:59 +0000

    Deploying to gh-pages from @ cedrodro/cedrodro.github.io@016cec55377c1015a8119d1883a60f37ff1125eb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "",
+          description: "including preprints",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio/publications/";
